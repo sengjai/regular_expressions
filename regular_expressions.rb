1862c8326f04a171cf00177e0b50b00659b11047
Soon Seng <soonseng.ee@thetechyhub.com>
2016-09-06 22:33:19 +0800

    Fixed all bugs

--- regular_expressions.rb
+++ regular_expressions.rb
@@ -3,7 +3,7 @@
 # Determine whether a string contains a Social Security number.
 def has_ssn?(string)
 	#byebug
-	if /\d{3}\-\d{2}\-\d{4}/.match(string) 
+	if /\d{3}\-\d{2}\-\d{4}/.match(string) #
 		return true
 	else
 		return false
@@ -19,7 +19,11 @@ puts has_ssn?("please confirm your identity: XXX-XX-1422") == false
 
 # Return the Social Security number from a string.
 def grab_ssn(string)
-	return /\d{3}\-\d{2}\-\d{4}/.match(string).to_s 
+	if /\d{3}\-\d{2}\-\d{4}/.match(string)
+		return /\d{3}\-\d{2}\-\d{4}/.match(string).to_s
+	else
+		return nil
+	end
 end
 
 
@@ -32,14 +36,11 @@ puts grab_ssn("please confirm your identity: XXX-XX-1422") == nil
 
 # Return all of the Social Security numbers from a string.
 def grab_all_ssns(string)
-	arr = []
-
-	#if /\d{3}\-\d{2}\-\d{4}/.match(string)
-	#	arr << /\d{3}\-\d{2}\-\d{4}/.match(string).to_s
-	#end
-	#p arr
-
-	return string.scan(/\d{3}\-\d{2}\-\d{4}/)
+	if /\d{3}\-\d{2}\-\d{4}/.match(string)	
+		return string.scan(/\d{3}\-\d{2}\-\d{4}/)
+	else
+		return []
+	end
 end
 
 puts "grab_all_ssns returns all SSNs if the string has any SSNs"
@@ -51,10 +52,14 @@ puts grab_all_ssns("please confirm your identity: XXX-XX-1422") == []
 
 # Obfuscate all of the Social Security numbers in a string. Example: XXX-XX-4430.
 def hide_all_ssns(string)
-
 	arr = []
-	string 
-	arr = string.gsub!(/\d{3}\-\d{2}/,"XXX-XX")
+
+	if /\d{3}\-\d{2}\-\d{4}/.match(string) #at least one ssn
+		arr = string.gsub!(/\d{3}\-\d{2}/,"XXX-XX")
+		return arr
+	else
+		return string
+	end
 end
 
 puts "hide_all_ssns obfuscates any SSNs in the string"
@@ -68,6 +73,12 @@ puts hide_all_ssns(string) == string
 # Ensure all of the Social Security numbers use dashes for delimiters.
 # Example: 480.01.4430 and 480014430 would both be 480-01-4430.
 def format_ssns(string)
+	#go home do .?
+	if /\d{3}\.?\d{2}\.?\d{4}/.match(string) #.? is a placeholder
+		string.gsub(/(\d{3})\W?(\d{2})\W?(\d{4})/, '\1-\2-\3') # \W - not word /placeholder ||  \1-\2-\3 =  
+	else
+		return string
+	end
 end
 
 puts "format_ssns finds and reformat any SSNs in the string"
